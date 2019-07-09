a300011afae3c65953cea1388a2a6aefa4d1bdbd
Gabriel Prá <gabrielpra1@gmail.com>
2019-07-09 09:49:49 -0300

    Allow Uploadex.get_files_url to use the definition's get_files and add documentation

--- lib/uploadex.ex
+++ lib/uploadex.ex
@@ -6,6 +6,10 @@ defmodule Uploadex do
   alias Ecto.Multi
   alias Uploadex.Files
 
+  @doc """
+  Inserts the changeset and store the record files in a database transaction,
+  so if the files fail to be stored the record will not be created.
+  """
   def create_with_file(changeset, uploader) do
     repo = uploader.repo
 
@@ -16,6 +20,12 @@ defmodule Uploadex do
     |> convert_result()
   end
 
+  @doc """
+  Updates the record and its files in a database transaction,
+  so if the files fail to be stored the record will not be created.
+
+  This function also deletes files that are no longer referenced.
+  """
   def update_with_file(changeset, previous_record, uploader) do
     repo = uploader.repo
 
@@ -27,6 +37,9 @@ defmodule Uploadex do
     |> convert_result()
   end
 
+  @doc """
+  Similar to `update_with_file/3`, but does not delete previous files.
+  """
   def update_with_file(changeset, uploader) do
     repo = uploader.repo
 
@@ -37,6 +50,10 @@ defmodule Uploadex do
     |> convert_result()
   end
 
+  @doc """
+  Deletes the record and all of its files.
+  This is not in a database transaction, since the delete operation never returns errors.
+  """
   def delete_with_file(record, uploader) do
     repo = uploader.repo
 
@@ -50,15 +67,44 @@ defmodule Uploadex do
   defp convert_result({:ok, %{insert: record}}), do: {:ok, record}
   defp convert_result({:ok, %{update: record}}), do: {:ok, record}
 
-  def get_file_url(_uploader, _record, nil, _endpoint_url), do: nil
+  @doc """
+  Returns the record's files (as defined in the definition `get_files`) URL, replacing the `uploader.base_directory()` with the endpoint URL.
 
-  def get_file_url(uploader, record, file, endpoint_url) when is_binary(endpoint_url) do
-    {file, record}
-    |> uploader.url()
-    |> String.replace(uploader.base_directory(), endpoint_url)
+  To provide the endpoint URL, there are two options:
+
+    * Provide the `endpoint_url` directly as a string
+    * Provide the `Phoenix.Endpoint` module
+  """
+  def get_files_url(uploader, record, endpoint_url) when is_binary(endpoint_url) do
+    record
+    |> uploader.do_get_files()
+    |> Enum.map(fn file ->
+      {file, record}
+      |> uploader.url()
+      |> String.replace(uploader.base_directory(), endpoint_url)
+    end)
   end
 
-  def get_file_url(uploader, record, file, endpoint) when is_atom(endpoint) do
-    get_file_url(uploader, record, file, endpoint.url())
+  def get_files_url(uploader, record, endpoint) when is_atom(endpoint) do
+    get_files_url(uploader, record, endpoint.url())
+  end
+
+  @doc """
+  Similar to `get_files_url/3`, but instead of returning all files defined in the definition `get_files`, returns the URL for the specified files.
+
+  This is useful when one record has multiple file fields.
+  """
+  def get_files_url(uploader, record, files, endpoint_url) when is_binary(endpoint_url) do
+    files
+    |> List.wrap()
+    |> Enum.map(fn file ->
+      {file, record}
+      |> uploader.url()
+      |> String.replace(uploader.base_directory(), endpoint_url)
+    end)
+  end
+
+  def get_files_url(uploader, record, files, endpoint) when is_atom(endpoint) do
+    get_files_url(uploader, record, files, endpoint.url())
   end
 end
