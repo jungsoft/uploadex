79c1e24c4ea57b7721028888e1eb61b3c4bd8bc9
Gabriel Prá <gabrielpra1@gmail.com>
2019-09-09 14:47:18 -0300

    Add Repo options to Uploadex functions

--- lib/uploadex.ex
+++ lib/uploadex.ex
@@ -10,11 +10,11 @@ defmodule Uploadex do
   Inserts the changeset and store the record files in a database transaction,
   so if the files fail to be stored the record will not be created.
   """
-  def create_with_file(changeset, uploader) do
+  def create_with_file(changeset, uploader, opts \\ []) do
     repo = uploader.repo
 
     Multi.new()
-    |> Multi.run(:insert, fn _repo, _ -> repo.insert(changeset) end)
+    |> Multi.run(:insert, fn _repo, _ -> repo.insert(changeset, opts) end)
     |> Multi.run(:store_files, fn _repo, %{insert: record} -> Files.store_files(record, uploader) end)
     |> repo.transaction()
     |> convert_result()
@@ -26,11 +26,11 @@ defmodule Uploadex do
 
   This function also deletes files that are no longer referenced.
   """
-  def update_with_file(changeset, previous_record, uploader) do
+  def update_with_file_delete_previous(changeset, previous_record, uploader, opts \\ []) do
     repo = uploader.repo
 
     Multi.new()
-    |> Multi.run(:update, fn _repo, _ -> repo.update(changeset) end)
+    |> Multi.run(:update, fn _repo, _ -> repo.update(changeset, opts) end)
     |> Multi.run(:store_files, fn _repo, %{update: record} -> Files.store_files(record, uploader) end)
     |> Multi.run(:delete_file, fn _repo, %{update: record} -> Files.delete_previous_files(record, previous_record, uploader) end)
     |> repo.transaction()
@@ -40,11 +40,11 @@ defmodule Uploadex do
   @doc """
   Similar to `update_with_file/3`, but does not delete previous files.
   """
-  def update_with_file(changeset, uploader) do
+  def update_with_file(changeset, uploader, opts \\ []) do
     repo = uploader.repo
 
     Multi.new()
-    |> Multi.run(:update, fn _repo, _ -> repo.update(changeset) end)
+    |> Multi.run(:update, fn _repo, _ -> repo.update(changeset, opts) end)
     |> Multi.run(:store_files, fn _repo, %{update: record} -> Files.store_files(record, uploader) end)
     |> repo.transaction()
     |> convert_result()
@@ -54,10 +54,10 @@ defmodule Uploadex do
   Deletes the record and all of its files.
   This is not in a database transaction, since the delete operation never returns errors.
   """
-  def delete_with_file(record, uploader) do
+  def delete_with_file(record, uploader, opts \\ []) do
     repo = uploader.repo
 
-    case repo.delete(record) do
+    case repo.delete(record, opts) do
       {:ok, record} -> Files.delete_files(record, uploader)
       {:error, error} -> {:error, error}
     end
