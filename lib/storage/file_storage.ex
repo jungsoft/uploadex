8b4fc77c6e964217b0ff8e1431d09a6bd2d6e692
Gabriel Prá <gabrielpra1@gmail.com>
2019-09-19 10:59:22 -0300

    Fix FileStorage get_url

--- lib/storage/file_storage.ex
+++ lib/storage/file_storage.ex
@@ -23,39 +23,39 @@ defmodule Uploadex.FileStorage do
 
   @impl true
   def store(file, opts) do
-    full_path = get_full_path(opts)
+    full_path = get_full_path_directory(opts)
 
     File.mkdir_p!(full_path)
     store_file(file, full_path)
   end
 
-  defp store_file(%{filename: filename, path: path}, directory), do: File.cp(path, full_path(filename, directory))
-  defp store_file(%{filename: filename, binary: binary}, directory), do: File.write(full_path(filename, directory), binary)
-
-  defp full_path(filename, directory), do: Path.join(directory, filename)
+  defp store_file(%{filename: filename, path: path}, directory), do: File.cp(path, Path.join(directory, filename))
+  defp store_file(%{filename: filename, binary: binary}, directory), do: directory |> Path.join(filename) |> File.write(binary)
 
   @impl true
   def delete(%{filename: filename}, opts), do: delete(filename, opts)
   def delete(filename, opts) when is_binary(filename) do
-    full_path = get_full_path(opts)
-
-    filename
-    |> full_path(full_path)
+    opts
+    |> get_full_path_directory()
+    |> Path.join(filename)
     |> File.rm()
   end
 
   @impl true
   def get_url(%{filename: filename}, opts), do: get_url(filename, opts)
   def get_url(filename, opts) when is_binary(filename) do
-    directory = Keyword.fetch!(opts, :directory)
+    full_path =
+      opts
+      |> get_full_path_directory()
+      |> Path.join(filename)
+
     base_path = Keyword.fetch!(opts, :base_path)
     base_url = Keyword.fetch!(opts, :base_url)
-    path = full_path(filename, directory)
 
-    String.replace(path, base_path, base_url)
+    String.replace(full_path, base_path, base_url)
   end
 
-  defp get_full_path(opts) do
+  defp get_full_path_directory(opts) do
     base_path = Keyword.fetch!(opts, :base_path)
     directory = Keyword.fetch!(opts, :directory)
     Path.join(base_path, directory)
