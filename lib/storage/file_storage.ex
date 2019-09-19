92ea9594d9accd418db26ed7daa78f28e0da15cc
Gabriel Prá <gabrielpra1@gmail.com>
2019-09-19 10:41:54 -0300

    File storage expects directory to be relative to base_path

--- lib/storage/file_storage.ex
+++ lib/storage/file_storage.ex
@@ -1,21 +1,32 @@
 defmodule Uploadex.FileStorage do
   @moduledoc """
-  File storage.
+  Storage for AWS S3.
 
-  opts:
-    directory: string!
-    base_path: string! for get_url
-    base_url: string! for get_url
+  ## Opts
+
+  * `directory`: String (required for all functions) - Relative to `base_path`
+  * `base_path`: String (required for all functions)
+  * `base_url`:  String (required for `c:Uploadex.Storage.get_url/2`)
+
+  To build the URL, `base_path` will be replaced by `base_url`.
+
+  ## Example
+
+    To use this storage for your `User` record, define these functions in your `Uploadex.Uploader` implementation:
+
+      def default_opts(Uploadex.FileStorage), do: [base_path: :code.priv_dir(:my_app), base_url: Endpoint.url()]
+
+      def storage(%User{} = user), do: {Uploadex.FileStorage, directory: "/uploads/users"}
   """
 
   @behaviour Uploadex.Storage
 
   @impl true
   def store(file, opts) do
-    directory = Keyword.fetch!(opts, :directory)
+    full_path = get_full_path(opts)
 
-    File.mkdir_p!(directory)
-    store_file(file, directory)
+    File.mkdir_p!(full_path)
+    store_file(file, full_path)
   end
 
   defp store_file(%{filename: filename, path: path}, directory), do: File.cp(path, full_path(filename, directory))
@@ -26,10 +37,10 @@ defmodule Uploadex.FileStorage do
   @impl true
   def delete(%{filename: filename}, opts), do: delete(filename, opts)
   def delete(filename, opts) when is_binary(filename) do
-    directory = Keyword.fetch!(opts, :directory)
+    full_path = get_full_path(opts)
 
     filename
-    |> full_path(directory)
+    |> full_path(full_path)
     |> File.rm()
   end
 
@@ -42,5 +53,11 @@ defmodule Uploadex.FileStorage do
     path = full_path(filename, directory)
 
     String.replace(path, base_path, base_url)
+  end
+
+  defp get_full_path(opts) do
+    base_path = Keyword.fetch!(opts, :base_path)
+    directory = Keyword.fetch!(opts, :directory)
+    Path.join(base_path, directory)
   end
 end
