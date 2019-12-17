957b799478840fda935a1764d51411ca807495bc
Gabriel Prá <gabrielpra1@gmail.com>
2019-12-17 11:37:36 +0100

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uploadex.MixProject do
   def project do
     [
       app: :uploadex,
-      version: "1.0.4",
+      version: "1.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
