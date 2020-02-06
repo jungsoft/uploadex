1f5e607cdcfba260427d6d7ddf4a74d904a867e2
Gabriel Prá <gabrielpra1@gmail.com>
2020-02-06 11:18:30 -0300

    Bump version to 2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uploadex.MixProject do
   def project do
     [
       app: :uploadex,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
