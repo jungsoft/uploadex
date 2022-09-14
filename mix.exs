ffe9531c664b995baa740f0bf911456d91dca792
João Otávio Biondo <joao.otb@gmail.com>
2022-09-14 20:46:31 +0200

    Update ex_doc to latest version

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Uploadex.MixProject do
       {:hackney, ">= 1.9.0", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
       # Runtime checks and doc
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, github: "gabrielpra1/dialyxir", only: [:dev, :test], runtime: false},
       # Temporary files
